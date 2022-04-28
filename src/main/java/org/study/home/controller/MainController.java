3762542e63bae2000f5cb978db5347b03ffafffa
heb <eunbnm5563@naver.com>
2022-04-28 14:26:51 +0900

    FrogProject 시작입니닷

---
+++ src/main/java/org/study/home/controller/MainController.java
@@ -0,0 +1,23 @@
+package org.study.home.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+	@GetMapping("/")
+	public String index() {
+		return "index";
+	}
+	
+	@GetMapping("/community")
+	public String community() {
+		return "board/community";
+	}
+	
+	@GetMapping("/gameInfo")
+	public String gameInfo() {
+		return "info/gameInfo";
+	}
+}
